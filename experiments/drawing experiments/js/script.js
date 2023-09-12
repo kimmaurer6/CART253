39e064a1b084bfb40b0a1a2bbf82f3f35cbfe049
Kimberley Maurer <kimmaurer6@yahoo.com>
2023-09-11 22:31:14 -0400

    Drawing experiment : added colour to my cone

--- experiments/drawing experiments/js/script.js
+++ experiments/drawing experiments/js/script.js
@@ -23,13 +23,27 @@ function setup() {
    createCanvas(500,500);
    
     background(29,22,59);
+    
+    noStroke();
 
     ellipseMode(CORNER);
+    
+    fill(128,0,38,100);
     ellipse(250,250,100,100);
+    
+    fill(138,10,48,100);
     ellipse(250,250,80,80);
+    
+    fill(148,20,58,100);
     ellipse(250,250,60,60);
+    
+    fill(158,30,68,100)
     ellipse(250,250,60,60);
+    
+    fill(168,40,78,100);
     ellipse(250,250,40,40);
+
+    fill(178,50,88,100);
     ellipse(250,250,20,20);   
 }
 
