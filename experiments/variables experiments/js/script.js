f832f2eb0c668a672053cbb040f2d1a398dfbc93
Kimberley Maurer <kimmaurer6@yahoo.com>
2023-09-18 10:38:11 -0400

    learned about map and constrain

--- experiments/variables experiments/js/script.js
+++ experiments/variables experiments/js/script.js
@@ -11,11 +11,11 @@
 let backgroundShade = 0;
 
 let circle = {
-    x: 250,
+    x: 0,
     y: 250,
     size: 100,
     speed: 1,
-    fill: 
+    fill: 255
 };
 
 /**
@@ -33,7 +33,9 @@ function draw() {
     background(backgroundShade);
 
     circle.x = circle.x + circle.speed;
+    circle.x = constrain(circle.x,0,width);
 
+    circle.fill = map(circle.x,0,width,0,255);
     fill(circle.fill);
     ellipse(circle.x, circle.y, circle.size);
 }
