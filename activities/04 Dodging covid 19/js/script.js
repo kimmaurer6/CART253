6918d6e52d53d453a84f3dd6adb91c2180a6ebf3
Kimberley Maurer <kimmaurer6@yahoo.com>
2023-09-24 21:50:42 -0400

    started Dodging Covid 19 activity

---
+++ activities/04 Dodging covid 19/js/script.js
@@ -0,0 +1,29 @@
+/**
+ * Dodging covid 19
+ * Kimberley Maurer
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
