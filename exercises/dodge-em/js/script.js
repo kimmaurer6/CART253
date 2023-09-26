b786ce1f6db9012e465696388491036d4f7fc4fd
Kimberley Maurer <kimmaurer6@yahoo.com>
2023-09-25 23:46:14 -0400

    E2 : worked on dodge-em activity

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -13,7 +13,7 @@ let circle1 = {
     size: 100,
     vx: 0,
     vy: 0,
-    speed: 1,
+    speed: 4,
     fill: {
         r: 195,
         g: 177,
@@ -35,12 +35,22 @@ let circle2 = {
     }
 };
 
+let user = {
+    x: 250,
+    y: 250,
+    size: 100,
+    fill: {
+        r: 250,
+        g: 200,
+        b: 152
+    }
+};
+
 /**
  * Description of setup
 */
 function setup() {
     createCanvas(windowWidth,windowHeight);
-    background(0,0,90)
 
     circle1.y = random(0,height);
     circle1.vx = circle1.speed;
@@ -53,7 +63,7 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-
+    background(0,0,90)
     // starry background
     for(let i; i < numStatic; i++);{
         let x = random(0,width);
@@ -85,13 +95,35 @@ function draw() {
     ellipse(circle2.x,circle2.y,circle2.size);
     pop();
 
-    // movement of the y circle we're dodging
-    // circle2.x = circle2.x + circle2.vx;
-    // circle2.y = circle2.y + circle2.vy;
+   // movement of the y circle we're dodging
+    circle2.x = circle2.x + circle2.vx;
+    circle2.y = circle2.y + circle2.vy;
 
-    // if(circle2.y > width){
-    //     circle2.y = 0;
-    //     circle2.x = random(0,height);
-    // };
+    if(circle2.y > height){
+        circle2.y = 0;
+        circle2.x = random(0,width);
+    };
+     
+    // show user
+    fill(user.fill.r, user.fill.g, user.fill.b);
+    noStroke();
+    ellipse(user.x,user.y,user.size);
 
+    // getting caught by the x circle! 
+    let d1 = dist(user.x,user.y,circle1.x,circle1.y);
+    if(d1 < circle1.size/2 + user.size/2){
+        noLoop();
+    };
+
+    // getting caught by the y circle! 
+    let d2 = dist(user.x,user.y,circle2.x,circle2.y);
+    if(d2 < circle2.size/2 + user.size/2){
+        noLoop();
+    };
+}
+
+function mouseDragged(){
+   // controlling the user by dragging the mouse
+    user.x = mouseX;
+    user.y = mouseY;
 }
