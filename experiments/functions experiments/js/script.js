198a28eafcfb132a471bbdd0eccdc5483e84cdd4
Kimberley Maurer <kimmaurer6@yahoo.com>
2023-10-01 17:48:50 -0400

    learned about return values

--- experiments/functions experiments/js/script.js
+++ experiments/functions experiments/js/script.js
@@ -13,7 +13,15 @@
 */
 function setup() {
     createCanvas(500,500); // defining the setup function, instructions
+
+    let hotCelsius = toCelsius(100);
+    console.log('100 degrees Fahrenheit is ${hotCelsius} degrees Celsius');
+
+    let coldCelsius = toCelsius(10);
+    console.log('10 degrees Fahrenheit is ${coldCelsius} degrees Celsius');
+
 }
+
 
 
 /**
@@ -21,19 +29,9 @@ function setup() {
 */
 function draw() {
     background(0);
-
-    parallels(100,100,5,1,100,1);
-    parallels(50,50,10,2,20,3);
-    parallels(200,200,15,7,3,12);
-    parallels(5,412,20,0.5,300,87);
 }
 
-function parallels(x,y, numLines, lineWidth, lineHeight, lineSpacing) {
-    for(let i = 0; i < numLines; i++) {
-        noStroke();
-        fill(255);
-        rectMode(CENTER);
-        rect(x,y,lineWidth,lineHeight);
-        x = x + lineSpacing;
-    }
+function toCelsius(fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5/9;
+    return celsius;
 }
