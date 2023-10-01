38a79b538dfbd1321128cdcd95cf02c5fa9dc229
Kimberley Maurer <kimmaurer6@yahoo.com>
2023-10-01 10:40:27 -0400

    learned about creating new functions

--- experiments/functions experiments/js/script.js
+++ experiments/functions experiments/js/script.js
@@ -8,13 +8,19 @@
 
 "use strict";
 
+let circle = {
+    x: 0,
+    y: 250,
+    size: 100,
+    vx: 1,
+    vy: 0
+};
+
 /**
  * Description of setup
 */
 function setup() {
-    createCanvas(500,500);
-    
-
+    createCanvas(500,500); // defining the setup function, instructions
 }
 
 
@@ -22,5 +28,36 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
 
+    move();
+    wrap();
+    display();
+}
+
+function move() {
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+}
+
+function wrap(){
+    if (circle.x > width){
+        reset();
+    }
+}
+
+function display (){
+    fill(250,0,0);
+    ellipse(circle.x, circle.y, circle.size);
+}
+
+function reset() { 
+    circle.x = 0;
+    circle.vx = circle.vx + 2;
+    circle.vy = circle.vy - 0.25;
+    circle.size = circle.size + 5;
+}
+
+function mousePressed() {
+    reset();
 }
