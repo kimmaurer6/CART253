e2d902e6f7711c2a1a3dc8f8c0607fb2d5f356f0
Kimberley Maurer <kimmaurer6@yahoo.com>
2023-10-15 20:45:46 -0400

    P1 : user control

--- projects/project 1/js/script.js
+++ projects/project 1/js/script.js
@@ -37,7 +37,13 @@ let circle2 = {
 }
 
 let user = {
-    x: 
+    x: windowWidth/2,
+    y: windowHeight/2,
+    size: 100,
+    vx: 0,
+    vy: 0,
+    speed: 3,
+    fill: 255
 }
 
 let state = `title`; // states are : title, game, saved, caught, foundLove
@@ -71,6 +77,10 @@ function setupCircles(){
 */
 function draw() {
 
+    ellipse(circle1.x, circle1.y, circle1.size);
+    // ellipse(circle2.x, circle2.y, circle2.size);
+    // ellipse(user.x, user.y, user.size);
+  
     if(state === `title`){
         title();
     }
@@ -89,6 +99,9 @@ function draw() {
 }
 
 function title(){
+    // background(135,206,235);
+    // noStroke();
+    
     // text for the title screen
     push();
     textSize(55);
@@ -103,9 +116,14 @@ function game(){
     // everything that needs to be called throughout the game
     move();
     controlUser();
+    display();
+    mousePressed();
 }
 
 function saved(){
+    background(250,200,152);
+    noStroke();
+
     // text for the safe ending
     push();
     textSize(55);
@@ -138,10 +156,71 @@ function foundLove(){
 }
 
 function move(){
-    // movement of the paparazzi/fans
+    // movement of the paparazzi on x axis
     circle1.x = circle1.x + circle1.vx;
     circle1.y = circle1.y + circle1.vy;
 
+    if (circle1.x > width) {
+        circle1.x = 0;
+        circle1.y = random(0,height);
+    }
+
+    // movement of the fans on y axis
     circle2.x = circle2.x + circle2.vx;
     circle2.y = circle2.y + circle2.vy;
+
+    if (circle2.y > height) {
+        circle2.y = 0;
+        circle2.x = random(0, width);
+    }
+}
+
+function paparazzi() {
+    let d = dist(circle1.x, circle1.y, user.x, user.y);
+    if(d < circle1.size/2 + user.size/2){
+        state = `caught`;
+    }
+}
+
+function fan(){
+    let d = dist(circle2.x, circle2.y, user.x, user.y);
+    if(d < circle2.size/2 + user.size/2){
+        state = `caught`;
+    }
+}
+
+function display(){
+    push();
+    fill(circle1.fill.r, circle1.fill.g, circle1.fill.b);
+    ellipse(circle1.x, circle1.y, circle1.size);
+    pop();
+
+    push();
+    fill(circle2.fill.r, circle2.fill.g, circle2.fill.b);
+    ellipse(circle2.x, circle2.y, circle2.size);
+    pop();
+}
+function controlUser(){
+    if(keyIsDown(87)){      // w to move up
+        user.vy = -user.speed;
+    }
+    else if(keyIsDown(83)){     // s to move down
+        user.vy = user.speed;
+    }
+    
+    if(keyIsDown(68)){      // d to move right
+        user.vx = user.speed;
+    }
+    else if(keyIsDown(65)){     // a to move left
+        user.vx = -user.speed;
+    }
+    user.x += user.vx;
+    user.y += user.vy;
+}
+
+function mousePressed(){
+    // when user clicks the title screen, the game starts!
+    if (state === `title`) {
+        state = `game`;
+    }
 }
