5f14ffcf6aa74028a75c0a56b3f0aadb7f40fc34
Kimberley Maurer <kimmaurer6@yahoo.com>
2023-10-23 23:57:00 -0400

    A6 : changed fish to butterflies and added control functions

--- activities/06 Age of Aquariums/js/script.js
+++ activities/06 Age of Aquariums/js/script.js
@@ -12,37 +12,45 @@ let user = {
     size: 100,
     vx: 0,
     vy: 0,
-    speed: 4
+    speed: 4,
+    fill: 255
 }
 
 let school = [];    // create an empty array and assign it to the school variable
 let imgButterfly;
-let imgUser;
+let imgCatcher;
 
 function preload(){
     imgButterfly = loadImage(`assets/images/butterfly.png`)
-    imgUser = loadImage(`assets/images/butterflycatcher.png`)
+    imgCatcher = loadImage(`assets/images/catchingbutterfly.png`)
 }
 
 function setup() {
     createCanvas(windowWidth,windowHeight);
 
-    school[0] = createFish(random(0, width), random(0, height));
-    school[1] = createFish(random(0, width), random(0, height));
-    school[2] = createFish(random(0, width), random(0, height));
-    school[3] = createFish(random(0, width), random(0, height));
-    school[4] = createFish(random(0, width), random(0, height));
-    school[5] = createFish(random(0, width), random(0, height));
-    school[6] = createFish(random(0, width), random(0, height));
-    school[7] = createFish(random(0, width), random(0, height));
+    school[0] = createButterfly(random(0, width), random(0, height));
+    school[1] = createButterfly(random(0, width), random(0, height));
+    school[2] = createButterfly(random(0, width), random(0, height));
+    school[3] = createButterfly(random(0, width), random(0, height));
+    school[4] = createButterfly(random(0, width), random(0, height));
+    school[5] = createButterfly(random(0, width), random(0, height));
+    school[6] = createButterfly(random(0, width), random(0, height));
+    school[7] = createButterfly(random(0, width), random(0, height));
+    school[8] = createButterfly(random(0, width), random(0, height));
+    school[9] = createButterfly(random(0, width), random(0, height));
+    school[10] = createButterfly(random(0, width), random(0, height));
+    school[11] = createButterfly(random(0, width), random(0, height));
+    school[12] = createButterfly(random(0, width), random(0, height));
 
 
+    // setting the x and y of the user
+    user.x = 50;
+    user.y = windowHeight/2;
 }
 
-// createFish(x,y)
-// creates a new javascript objects describing a fish and returns it
-function createFish(x,y){
-    let fish = {
+function createButterfly(x,y){
+    // creating the butterflies
+    let butterfly = {
         x: x,
         y: y,
         size: 50,
@@ -50,44 +58,74 @@ function createFish(x,y){
         vy: 0,
         speed: 2
     };
-    return fish;
+    return butterfly;
 }
 
 function draw() {
     background(0, 0, 139);
 
-    for(let i = 0; i < 8; i++){
-        moveFish(school[i]);
-        displayFish(school[i]);
-}
+    for(let i = 0; i < 12; i++){
+        moveButterfly(school[i]);
+        displayButterfly(school[i]);
 }
 
-// moveFish(fish)
-// chooses whether the providing fish changes direction and moves it
-function moveFish(fish){
+user.x = constrain(user.x, 0, windowWidth);
+user.y = constrain(user.y, 0, windowHeight);
+}
+
+function simulation(){
+    background(0, 0, 139);
+
+    // everything to be called
+    moveButterfly();
+    displayButterfly();
+    displayUser();
+    controlUser();
+}
+function moveButterfly(butterfly){
     //choose whether to change direction
     let change = random(0,1);
     if(change < 0.05){
-        fish.vx = random(-fish.speed, fish.speed);
-        fish.vy = random(-fish.speed, fish.speed);
+        butterfly.vx = random(-butterfly.speed, butterfly.speed);
+        butterfly.vy = random(-butterfly.speed, butterfly.speed);
     }
-    // move the fish
-fish.x = fish.x + fish.vx;
-fish.y = fish.y + fish.vy;
+    // move the butterfly
+butterfly.x = butterfly.x + butterfly.vx;
+butterfly.y = butterfly.y + butterfly.vy;
 
-// constrain the fish to the canvas
-fish.x = constrain(fish.x, 0, width);
-fish.y = constrain(fish.y, 0, height);
+// constrain the butterfly to the canvas
+butterfly.x = constrain(butterfly.x, 0, width);
+butterfly.y = constrain(butterfly.y, 0, height);
 }
 
-// displayFish(fish)
-// displays the provided fish on the canvas
-function displayFish(fish){
+// displayButterfly(butterfly)
+// displays the provided butterfly on the canvas
+function displayButterfly(butterfly){
     push();
-    image(imgButterfly, fish.x, fish.y, fish.size/1.5, fish.size/1.5);
+    image(imgButterfly, butterfly.x, butterfly.y, butterfly.size/1.5, butterfly.size/1.5);
     pop();
 }
 
 function displayUser(){
+    push();
+    image(imgCatcher, user.x, user.y, user.size/1.5, user.size/1.5);
+    pop();
+}
 
+function controlUser(){
+    if(keyIsDown(87)){
+        user.vy = -user.speed;
+    }
+    else if(keyIsDown(83)){
+        user.vy = user.speed;
+    }
+
+    if(keyIsDown(68)){
+        user.vx = user.speed;
+    }
+    else if(keyIsDown(65)){
+        user.vx = -user.speed;
+    }
+    user.x += user.vx;
+    user.y += user.vy;
 }
